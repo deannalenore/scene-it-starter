e3b2da2947cb37465e6ce736ca69b844042ce7bf
Deanna Hart <deanna.lenore96@gmail.com>
2020-03-11 14:10:31 -0400

    Part two updates

--- index.js
+++ index.js
@@ -6,32 +6,43 @@ document.addEventListener('DOMContentLoaded', function() {
         <div class = "card-body">
             <h3 class = "card-title"> ${currentMovie.Title}</h3>
             <h4 class = "card-subtitle mb-2 text-muted">${currentMovie.Year}</h4>
-            <button onclick = "saveToWatchlist(${currentMovie.imdbID})">Add</button>
+            <button onclick = "saveToWatchlist("${currentMovie.imdbID}")">Add</button>
         </div>
     </div>	`
-    })
+    }).join("")
 return movieTitles
    } 
-   var title = renderMovies(movieData)
-   console.log(title.join(''))
-   var moviesContainer = document.getElementById("movies")
-   document.getElementById("search-form").addEventListener("submit", function(e){
+
+   //    var moviesContainer = document.getElementById("movies")
+//    moviesContainer.innerHTML = renderMovies(movieData)
+   var form = document.getElementById("search-form")
+   form.addEventListener("submit", function(e){
        e.preventDefault();
+   var movieInput = e.target.elements[0].value.toUpperCase() 
+   var searchArray = movieData.filter((element)=>{
+       return element.Title.toUpperCase().includes(movieInput) 
+   })
+   console.log(searchArray)
+      var moviesContainer = document.getElementById("movies")
+   moviesContainer.innerHTML = renderMovies(searchArray)
 })
-    function saveToWatchlist(imdbID){
-        var movie = movieData.find(function(currentMovie){
-            return currentMovie.imdbID == imdbID;
-        });
-        var watchlistJSON = localStorage.getItem(watchlist);
-        var watchlist = JSON.parse(watchlistJSON);
+    
+        //var movie = movieData.find(function(currentMovie){
+            //return currentMovie.imdbID == imdbID;
+        //});
+        //var watchlistJSON = localStorage.getItem(watchlist);
+        //var watchlist = JSON.parse(watchlistJSON);
 
-        if (watchlist === null){
-            return watchlist = [];
-        }
+        //if (watchlist === null){
+            //return watchlist = [];
+        //}
         
-        watchlist.push(movie);
+        //watchlist.push(movie);
 
 
 
-    }
+    
 });
+function saveToWatchlist(imdbID){
+
+}
